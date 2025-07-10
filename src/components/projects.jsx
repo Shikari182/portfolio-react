0a8b37b6f96be3d8d2c7e212123783902497a2d7
Enrique <enriqueShikari@gmail.com>
2025-07-10 14:38:40 +0200

    Projects added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,6 +6,9 @@ import computadora from '../assets/images/computadora.png';
 const proyectos = [
   { id: 1, title: 'QA Testing', desc: 'Automatización de pruebas con Playwright', img: computadora },
   { id: 2, title: 'Desarrollo de aplicaciones web', desc: 'Implementación del frontend con versatilidad', img: computadora },
+  { id: 3, title: 'Desarrollo y diseño de videojuegos', desc: 'Diseño de proyectos relacionados con el mundo de los videojuegos y su implementación', img: computadora },
+  { id: 4, title: 'Páginas web bancarias', desc: 'Diseño de páginas web para entidades bancarias', img: computadora },
+  { id: 5, title: 'Aplicaciones interactivas para niños', desc: 'Creación de diversos juegos web con una interfaz sencilla para los más pequeños', img: computadora },
 ];
 
 export default function Projects() {
